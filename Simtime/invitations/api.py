0a679c5522095c32a5ce62bb2881c7474ae51e12
arara90 <ararajo@gmail.com>
2021-03-04 02:21:34 +0900

    cursor ing

--- Simtime/invitations/api.py
+++ Simtime/invitations/api.py
@@ -4,10 +4,10 @@ import tempfile
 from datetime import datetime
 from django.conf import settings
 from django.utils import timezone
-from django.db.models import Max
+from django.db.models import Max, FilteredRelation, Q
 from .models import Invitation, Event
 from .serializers import InvitationSerializer, EventSerializer, HostSerializer
-from accounts.models import Friendship
+from accounts.models import Friendship, Account
 
 # from .models import  Event
 # from .serializers import EventSerializer
@@ -110,11 +110,29 @@ class InvitationAPI(APIView):
         invitations = request.user.invitations\
             .select_related('event').filter(event__event_time__range=[start_datetime_aware, end_datetime_aware])\
 
-        for item in invitations:
-            if(item.event.host.id > request.user.id):
-                print('B_subscribe_A', item.event.host)
-            else:
-                print('A_subscribe_B', item.event.host)
+
+        from django.db import connection
+        def my_custom_sql():
+            with connection.cursor() as cursor:
+                cursor.execute(
+                '''select invitations_invitation.*
+                from public.invitations_invitation
+                left join public.accounts_friendship
+                on ( "invitations_invitation"."guest_id" = "accounts_friendship"."account_A_id"
+                    and  "accounts_friendship"."A_subscribe_B" )
+                where "invitations_invitation"."guest_id" =1
+
+                union all
+
+                select invitations_invitation.* 
+                from public.invitations_invitation
+                left join public.accounts_friendship
+                on ( "invitations_invitation"."guest_id" = "accounts_friendship"."account_B_id"
+                    and  "accounts_friendship"."B_subscribe_A" )
+                where "invitations_invitation"."guest_id" = 1''')
+                
+                columns = [col[0] for col in cursor.description]
+                return [dict(zip(columns, row)) for row in cursor.fetchall()]
 
         serializer = InvitationSerializer(invitations, many=True)
         return Response(serializer.data)
